1de1a0a803d44989c5270d655cf004f717b54ab8
aliemresafak <50514506+aliemresafak@users.noreply.github.com>
2022-04-19 10:51:58 +0000

    Deploying to gh-pages from @ enerjidev/enerjidev.github.io@01a1d3ab2c17261a36d6bd2696ef3dd8dc40fe96 🚀

---
+++ _next/static/KDMHCk1JfoJ-6tpUHP7dJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-11fd8f625bcdb8aa.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
